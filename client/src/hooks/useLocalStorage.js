4ad02926da3d54378f00d2b11d63ef3f072ce65a
KangerDrew <kangdrew95@gmail.com>
2022-07-13 10:42:04 -0700

    comments added for clarity

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -1,3 +1,6 @@
+// It seems, that instead of using database to keep track of user,
+// we are going to use localStorage to keep the user data & messaging
+// history:
 import { useEffect, useState } from 'react'
 
 
@@ -8,9 +11,14 @@ export default function useLocalStorage(key, initialValue) {
     const prefixedKey = PREFIX + key
     const [value, setValue] = useState(() => {
 
+        // Retrieve prefixedKey's value from localStorage JSON:
         const jsonValue = localStorage.getItem(prefixedKey)
-
+        
+        // If there are stored data already, just return it:
         if (jsonValue != null) return JSON.parse(jsonValue)
+        
+        // Otherwise, check initialValue type. If it's a function,
+        // return it as the function. Otherwise return it normally:
         if (typeof initialValue === 'function') {
             return initialValue()
         } else {
@@ -19,6 +27,8 @@ export default function useLocalStorage(key, initialValue) {
 
     })
 
+    // useEffect keeps track of when the prefixedKey or value changes,
+    // and changes the id stored in localStorage when they are updated:
     useEffect(() => {
         localStorage.setItem(prefixedKey, JSON.stringify(value))
     }, [prefixedKey, value])
